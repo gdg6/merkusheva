0dc0512c296c0fa1ec97f3acd4162458be028900
gdg6 <e4pj6vity>
2014-12-04 11:47:07 +0300

    Вторая лаба
    сделаны состояния и вспомогательные переменые для дальнейшего развития
    проекта

--- 2/main.cpp
+++ 2/main.cpp
@@ -1,16 +1,18 @@
 //программа для подсчета максимальной вложенности
 //будем считать что все функции и циклы имеют {}
-
-//программа работает след образом: на вход пуступают данные. Их рассматриваем как  данные с вершины стека.
-//внутри программы есть свой стек который будет хранить {} и в  конечном итогде в этом стеке необходимо будет найти максимальную вложенность.
+//программа для подсчета максимальной вложенности. 
+// и программа не предусмотренна на обработку кода в строке, вроде: "void func(){}"
+//и во входном листенге программы не учитываются комментарии. Т.к. не было сказано в задании.
 
 #include <iostream>
 #include <list>
 #include <algorithm>
+#include <vector>
 
 using std::cout;
 using std::endl;
 
+#define FOR_LENGHT 3
 
 class Machine
 {
@@ -20,17 +22,36 @@ class Machine
 		IF_STATE, 
 		DO_STATE, 
 		WHILE_STATE, 
-		FUNCTION_STATE
+		FUNCTION_STATE,
+		CLASS_STATE,
+		WAIT_OPEN_SCOBAL,
+		WAIT_CLOSE_SCOBAL
+		WAIT_OPEN_CIRC_SCOBAL,
+		WAIT_close_CIRC_SCOBAL
 	};
 	
 	//generals 
 	int currentNestingLevel = 0;
 	int maxNestingLevel = 0;
+	int state;
+	//контейней для скобок условной архитектуры вложенности. 
+	std::vector<char> nestingScobals;
 	
 	//helpers
+	int lenght_current_word = 0;
+	char before_be_space = true;
+	enum {BEFORE_SPACE};
+	int state_help;
 	
+	void checkOnFor(char c)
+	{
+		if(lenght_current_word > 3 && state_help == BEFORE_SPACE ) state
+	
+	}
 	
 public:
+
+	Machine() : state(START_STATE){}
 
 	void processState(char c)
 	{
